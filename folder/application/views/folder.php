5546fbad21545446af43db7367f64f8be66d35a3
maaikeongena <maaike.ongena@asadventure.com>
2015-04-29 15:31:03 +0200

    created includes

--- folder/application/views/folder.php
+++ folder/application/views/folder.php
@@ -5,6 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Folder maart 2015 - Werktitel</title>
 	<link rel="stylesheet" href="http://static.asadventure.com/includes/landingpage/lentefolder2015/stylesheets/css/folder-maart.css">
+	<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script> -->
 	<script src="http://static.asadventure.com/includes/landingpage/lentefolder2015/bower_components/lazyloadxt/dist/jquery.lazyloadxt.min.js"></script>
 	<script src="http://static.asadventure.com/includes/landingpage/lentefolder2015/bower_components/lazyloadxt/dist/jquery.lazyloadxt.bg.min.js"></script>
 	<script src="http://static.asadventure.com/includes/landingpage/lentefolder2015/scripts/filtering.min.js"></script>
@@ -12,59 +13,54 @@
 <body>
 	<div id="lp-folder-wrapper">
 		<!-- Header -->
-			<?php include 'includes/videowrapper.php'; ?>
-		<!-- filermenu -->
-			<?php include 'includes/filermenu.php'; ?>
+		<?php include 'includes/videowrapper.php'; ?>
+		<?php include 'includes/filermenu.php'; ?>
+
+
 		<!-- Eerste categoriehoofding -->
 		<div class="content-row z-shadow">
 			<img data-src="http://static.asadventure.com/includes/landingpage/lentefolder2015/images/tussentitels/<?php echo strtolower($categorie) ?>.jpg">
 		</div>
+
 		<!-- start eerste contentrow -->
 		<div class="content-row">
-		<!-- loop door producten -->
-		<?php foreach ($products as $product) { ?>
-			<!--Indien nieuwe categorie-->
-			<?php if ($product["categorie"] != $categorie) { ?>
-				<!-- rij afwerken -->
-				<?php if(( $count % 4 ) != 0 ) { 
-					if ($gridcount % 3 == 0) {
-						$photocount++;
-					}
-					$gridcount++; 
+
+			<!-- loop door producten -->
+			<?php foreach ($products as $product) { ?>
+				<!--Indien nieuwe categorie-->
+				<?php if ($product["categorie"] != $categorie) { ?>
+					<!-- rij afwerken -->
+					<?php if(( $count % 4 ) != 0 ) { 
+						if ($gridcount % 3 == 0) {
+							$photocount++;
+						}
+						$gridcount++; 
 					} ?>
-				<?php while(( $count % 4 ) != 0 ) { 
-					 include 'includes/foto.php';?>
-					<div class="col-3 row-1 product-container lookbook"></div>
-					<?php $count++;
-					} ?>
+					<?php while(( $count % 4 ) != 0 ) { ?>
+							<?php include 'includes/foto.php'; ?>
+							<div class="col-3 row-1 product-container lookbook"></div>
+							<?php $count++;} ?>
+						</div>
+					<!-- nieuwe rij initialiseren -->
+					<div class="content-row z-shadow">
+					<!-- categorie herdefiniëren -->
+						<?php $categorie = $product["categorie"]; ?>
+						<img data-src="http://static.asadventure.com/includes/landingpage/lentefolder2015/images/tussentitels/<?php echo strtolower($categorie) ?>.jpg">
 					</div>
-				<!-- nieuwe rij initialiseren -->
-				<div class="content-row z-shadow">
-				<!-- categorie herdefiniëren -->
-					<?php $categorie = $product["categorie"]; ?>
-			<img data-src="http://static.asadventure.com/includes/landingpage/lentefolder2015/images/tussentitels/<?php echo strtolower($categorie) ?>.jpg">
-								</div>
-				<div class="content-row">
+					<div class="content-row">
 				<!-- end if -->
 				<?php	} ?>
-			<!-- print foto -->
+				<!-- print foto -->
 				<?php include 'includes/foto.php'; ?>
-				<?php include 'includes/printproduct.php';?>
-			<?php if ( $count % 4 == 0 ) { ?>
-				</div>
-				<div class="content-row">
-				<?php 
-					if ($gridcount % 3 == 0) {
-						$photocount++;
-					}
-					$gridcount++;
-			} ?>
-		<!-- end loop -->
-		<?php	} ?>
+				<!-- print gridcontainer -->
+			    <?php include 'includes/printproduct.php'; ?>
+			<!-- end loop -->
+			<?php	} ?>
+
 		<!-- maak laatste rij af -->
-		<?php while(( $count % 4 ) != 0 ) { 
-			include 'includes/foto.php'; ?>
-		<div class="col-3 row-1 product-container lookbook"></div>
+		<?php while(( $count % 4 ) != 0 ) { ?>
+			<?php include 'includes/foto.php'; ?>
+			<div class="col-3 row-1 product-container lookbook"></div>
 		<?php $count++;
 		} ?>
 		</div>
