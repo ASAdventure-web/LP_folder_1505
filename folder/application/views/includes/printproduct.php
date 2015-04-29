22da01d0ee6f87f971a54ef54b27d010b663afec
Bert van Depoele <bert.vandepoele@asadventure.com>
2015-04-29 14:34:22 +0200

    Includes toevoegen

---
+++ folder/application/views/includes/printproduct.php
@@ -0,0 +1,56 @@
+			<!-- print lookbook image -->
+			<?php if(isset($product['lookbook'])) { ?>
+				<div class="col-3 row-1 product-container lookbook" data-bg="http://static.asadventure.com/includes/landingpage/lentefolder2015/images/producten/<?php echo $product['img'] ?>">
+					<a href="<?php echo $product['link'] ?>">
+						<span>
+							<?php echo $product['productnaam'] ?>
+						</span>
+						<button>
+							Naar de lookbook
+						</button>
+			<?php	}  ?>
+
+			<!-- print product -->
+
+			<?php else {?>
+
+			<div class="col-3 row-1 product-container <?php if(isset($product['exclusief'])) { print(' exclusive-nl'); } if (isset($product['uitverkocht'])) { print(' sold-out-nl'); }?>">
+				<a href="<?php echo $product['link'] ?>" alt="<?php echo $product['productnaam'] ?>" class="contentlink">
+					<img class="product-image" data-src="http://static.asadventure.com/includes/landingpage/lentefolder2015/images/producten/<?php echo $product['img'] ?>" title="<?php echo $product['productnaam'].' '.$product['productomschrijving'] ?>">
+					<div class="bottom-container">
+						<div class="price-tag">
+							<?php if(isset($product['specialeactie'])) { ?>
+							<p class="special"><?php echo $product['specialeactie'] ?></p>
+							<?php } 
+							if(isset($product['oudeprijs'])) { ?>
+							<p class="barred-price">&euro; <?php echo $product['oudeprijs'] ?></p>
+							<?php } ?>
+							<?php if(isset($product['nieuweprijs'])) { ?>
+							<p class="new-price"><span class="currency">&euro;</span><?php echo $product['nieuweprijs'] ?></p>	
+							<?php } ?>
+						</div>
+						<h3><?php echo $product['productnaam'] ?> <span class="product-category"><?php echo $product['productomschrijving'] ?></span></h3>
+						
+						<div class="logo-container">
+							<img data-src="http://static.asadventure.com/includes/landingpage/lentefolder2015/images/merken/<?php echo $product['logo'] ?>" alt="<?php echo $product['merknaam'] ?>" class="logo">
+							<?php if(isset($product['kleur'])) { ?>
+							<div class="swatch-container">
+								<?php $kleuren = explode(" ", $product['kleur']); 
+									foreach ($kleuren as $kleur) { 
+										if ( $kleur[0] == '#') {?>
+										<span class="swatch" style="background: <?php echo $kleur ?>"></span>								
+								<?php 	}
+										else { ?>
+											<span class="swatch" data-bg="http://static.asadventure.com/includes/landingpage/lentefolder2015/images/kleuren/<?php echo $kleur ?>"></span>
+										<?php }
+								}
+								?>
+							</div>
+						<?php } ?>
+						</div>
+					</div>
+					<?php } ?>
+				</a>
+			</div>
+			<?php $count++; ?>
+			<!--  sluit rij indien nodig -->
---
+++ folder/application/views/includes/videowrapper.php
@@ -0,0 +1,3 @@
+<div class="video-wrapper">
+			<iframe src="https://player.vimeo.com/video/122988356" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+		</div>
