ae6973831924feb6618b38c32ab39c0c28a79e32
maaikeongena <maaike.ongena@asadventure.com>
2015-05-20 16:41:20 +0200

    fixed problems grid counter

--- folder/application/views/filter2.php
+++ folder/application/views/filter2.php
@@ -17,19 +17,22 @@
 				<!--Indien nieuwe categorie-->
 				<?php if ((isset($product[$secondfilter])) && ($categorie !== $secondfilter)) { ?>
 					<!-- rij afwerken -->
-					<?php if(( $count % 4 ) != 0 ) { 
-						if ($gridcount % 3 == 0) {
-							$photocount++;
-						}
-						$gridcount++; 
-					} ?>
+
 					<?php while(( $count % 4 ) != 0 ) { ?>
 							<?php include 'includes/foto.php'; ?>
 							<div class="col-3 row-1 product-container lookbook"></div>
-							<?php $count++;} ?>
-						</div>
-					<!-- nieuwe rij initialiseren -->
-					<div class="content-row z-shadow" id="<?php print($secondfilter);?>">
+							<?php $count++;
+							if ( $count % 4 == 0 ) {
+									if ($gridcount % 3 == 0) {
+										$photocount++;
+									}
+									$gridcount++; }
+							}
+
+							 ?>
+							</div>
+
+							<div class="content-row z-shadow" id="<?php print($secondfilter);?>">
 					<!-- categorie herdefiniëren -->
 						<?php $categorie = $secondfilter; ?>
 						<img data-src="http://static.asadventure.com/includes/landingpage/2015_zomerfolder/images/tussentitels/<?php echo strtolower($categorie) ?>.jpg">
@@ -43,6 +46,13 @@
 			    <?php include 'includes/printproduct.php'; ?>
 			<!-- end loop -->
 			<?php	} ?>
+
+			<!-- maak laatste rij af -->
+		<?php while(( $count % 4 ) != 0 ) { ?>
+			<?php include 'includes/foto.php'; ?>
+			<div class="col-3 row-1 product-container lookbook"></div>
+		<?php $count++;
+		} ?>
 		</div>
 	</div>
 </body>
