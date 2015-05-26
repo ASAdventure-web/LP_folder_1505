85c8238a1923994bd96cf10b9487aea607a922f3
Bert van Depoele <bert.vandepoele@asadventure.com>
2015-05-26 17:24:06 +0200

    overlay

--- folder/application/views/includes/links.php
+++ folder/application/views/includes/links.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="http://static.asadventure.com/includes/landingpage/2015_zomerfolder/stylesheets/css/folder-juni.css">
-
-<!-- <link rel="stylesheet" href="../../../stylesheets/css/folder-juni.css">
- --><script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+ 
+<!--<link rel="stylesheet" href="../../../stylesheets/css/folder-juni.css"> -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script src="http://static.asadventure.com/includes/landingpage/2015_zomerfolder/bower_components/lazyloadxt/dist/jquery.lazyloadxt.min.js"></script>
 <script src="http://static.asadventure.com/includes/landingpage/2015_zomerfolder/bower_components/lazyloadxt/dist/jquery.lazyloadxt.bg.min.js"></script>
 <script src="http://static.asadventure.com/includes/landingpage/2015_zomerfolder/javascript/filtering.min.js"></script>
