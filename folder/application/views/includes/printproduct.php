e0f68fc846f255bd0d3b365445f8637bf2952690
maaikeongena <maaike.ongena@asadventure.com>
2015-05-27 13:40:52 +0200

    display 2+1 overlay everywhere

--- folder/application/views/includes/printproduct.php
+++ folder/application/views/includes/printproduct.php
@@ -19,7 +19,7 @@
 		<?php } ?>
 <?php	}  else {?>
 <!-- print product -->
-<div class="col-3 row-1 product-container <?php if(isset($product['exclusief'])) { print(' exclusive-'.$lang.''); } if (isset($product['uitverkocht'])) { print(' sold-out-'.$lang.''); } if (isset($product['groeneprijs'])) { print(' groeneprijs-'.$lang.''); } if (isset($product['2plus1']) && $active != "2plus1") { print(' t-shirts'); }?>">
+<div class="col-3 row-1 product-container <?php if(isset($product['exclusief'])) { print(' exclusive-'.$lang.''); } if (isset($product['uitverkocht'])) { print(' sold-out-'.$lang.''); } if (isset($product['groeneprijs'])) { print(' groeneprijs-'.$lang.''); } if (isset($product['2plus1'])) { print(' t-shirts'); }?>">
 	<a href="<?php echo $product['link'] ?>" alt="<?php echo $product['productnaam'] ?>" class="contentlink">
 		<img class="product-image" data-src="http://static.asadventure.com/includes/landingpage/2015_zomerfolder/images/producten/<?php echo $product['img'] ?>" title="<?php echo $product['productnaam'].' '.$product['productomschrijving'] ?>">
 		<div class="bottom-container">
