dbbb9a75885a126898b70f35d35c81274f3bc006
maaikeongena <maaike.ongena@asadventure.com>
2015-05-28 08:51:54 +0200

    changed script link

--- folder/application/views/includes/links.php
+++ folder/application/views/includes/links.php
@@ -4,6 +4,6 @@
 <!-- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
  --><script src="http://static.asadventure.com/includes/landingpage/2015_zomerfolder/bower_components/lazyloadxt/dist/jquery.lazyloadxt.min.js"></script>
 <script src="http://static.asadventure.com/includes/landingpage/2015_zomerfolder/bower_components/lazyloadxt/dist/jquery.lazyloadxt.bg.min.js"></script>
-<script src="http://static.asadventure.com/includes/landingpage/2015_zomerfolder/javascript/filtering.min.js?v=2"></script>
+<script src="http://static.asadventure.com/includes/landingpage/2015_zomerfolder/javascript/filtering.min.js?v=3"></script>
 <!-- <script src="../../../javascript/filtering.js"></script>
  -->
